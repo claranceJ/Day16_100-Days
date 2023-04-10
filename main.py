65c2fac011f2af204503ce5cf269dac8adb56b18
Jacktone Clarance <77950647+claranceJ@users.noreply.github.com>
2023-04-10 07:57:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Fill in the blank Lyrics!")
+print("(Type in the blank lyrics and see if you are as cool as me.")
+print()
+count=1
+while True:
+  # get lyrics from user
+  print("Never going to_________you up.")
+  print()
+  lyrics=input("Enter missing lyrics:")
+  if lyrics !="give":
+    count+=1
+  elif lyrics=="give":
+    print(f"well done! It only took you {count} attempts" )
+  
+    
+    
